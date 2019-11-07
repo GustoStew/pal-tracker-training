85e756dd45b2bd7822798b3769a63abc65c98030
GustoStew <germain.leguen@gmail.com>
2019-11-07 16:22:38 +0100

    LAB 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
 
+    private final String welcomeMessage;
+
+    public WelcomeController( @Value("${welcome.message}") String welcomeMessage) {
+        this.welcomeMessage = welcomeMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "HELLO";
+        return welcomeMessage;
     }
 
 
